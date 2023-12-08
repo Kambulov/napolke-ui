2a8973321fea03c5a8b847a00effa5a3a5aa8ef4
d.kambulov@napolke.ru <d.kambulov@napolke.ru>
2023-12-08 18:11:35 +0300

    qwe

--- packages/icons/src/generate.ts
+++ packages/icons/src/generate.ts
@@ -13,11 +13,12 @@ import {
 } from './utils'
 
 const outputDir = path.join(__dirname, '../', 'dist')
-const sourceFile = path.join(__dirname, '../', '.source')
+const sourceFile = path.join(__dirname, '../', 'source')
 
 export default (async () => {
-  await fs.remove(outputDir)
   try {
+    console.log('tt',outputDir)
+    await fs.remove(outputDir)
     const files = fs.readdirSync(sourceFile)
     let exports = ''
     let definition = makeBasicDefinition()
@@ -51,7 +52,83 @@ export default ${fileName}\n`
     const allModulesCode = transform(exports, allModulesBabelConfig).code
     await fs.outputFile(path.join(outputDir, 'index.d.ts'), definition)
     await fs.outputFile(path.join(outputDir, 'index.js'), allModulesCode)
+    console.log('tttt',allModulesCode)
+
   } catch (error) {
     console.error(`Error reading SVG files: ${error.message}`);
   }
+
+
+//
+//   let exports = ''
+//   let definition = makeBasicDefinition()
+//
+//   const icons = document.querySelectorAll('.geist-list .icon')
+//   const svgo = new SVGO(svgoConfigs)
+//   const promises = Array.from(icons).map(async (icon: Element) => {
+//     const name: string = icon.querySelector('.geist-text').textContent
+//     const componentName = toComponentName(name)
+//     const fileName = toHumpName(name)
+//
+//     const svg = icon.querySelector('svg')
+//     const { data: optimizedSvgString } = await svgo.optimize(svg.outerHTML)
+//     const styles = parseStyles(svg.getAttribute('style'))
+//
+//     const component = `import React from 'react';
+// const ${componentName} = ({ color = 'currentColor', size = 24, style, ...props }) => {
+//   return ${parseSvg(optimizedSvgString, styles)};
+// }
+// export default ${componentName};`
+//
+//     exports += `export { default as ${componentName} } from './${fileName}';\n`
+//     definition += `export const ${componentName}: Icon;\n`
+//
+//     const componentDefinition = `${makeBasicDefinition()}declare const ${componentName}: Icon;
+// export default ${componentName}\n`
+//     const componentCode = transform(component, moduleBabelConfig).code
+//     await fs.outputFile(path.join(outputDir, `${fileName}.d.ts`), componentDefinition)
+//     await fs.outputFile(path.join(outputDir, `${fileName}.js`), componentCode)
+//   })
+//
+//   await Promise.all(promises)
+//   const allModulesCode = transform(exports, allModulesBabelConfig).code
+//   await fs.outputFile(path.join(outputDir, 'index.d.ts'), definition)
+//   await fs.outputFile(path.join(outputDir, 'index.js'), allModulesCode)
 })()
+
+const parseSvg = (svg: string, styles: any) => {
+  const getSpecifiedColorVar = (val: string | undefined, ident: string) => {
+    if (!val) return '""'
+    return val.includes(ident) ? '{color}' : '"var(--geist-icons-background)"'
+  }
+
+  svg = svg.replace(/-([a-z])(?=[a-z\-]*[=\s/>])/g, g => g[1].toUpperCase())
+  svg = svg.replace(/<svg([^>]+)>/, `<svg$1 {...props} height={size} width={size} style={{...style,color}}>`)
+
+  const geistFillColor = getSpecifiedColorVar(styles['--geist-fill'], 'current')
+  const geistStrokeColor = getSpecifiedColorVar(styles['--geist-stroke'], 'current')
+
+  // With Geist UI
+  // Refer to: https://github.com/geist-org/react/pull/139/files#diff-b174da32165cea69128b525762abb680R22
+  svg = replaceAll(svg, '"var(--geist-foreground)"', '{color}')
+  svg = replaceAll(svg, '"var(--geist-background)"', '"var(--geist-icons-background)"')
+
+  // Reset dynamic colors
+  // In a few icons, the semantics of 'fill' and 'stroke' are not correct,
+  // they maybe forced to override by style.
+  svg = replaceAll(svg, '"var(--geist-fill)"', geistFillColor)
+  svg = replaceAll(svg, '"var(--geist-stroke)"', geistStrokeColor)
+  return svg
+}
+
+const parseStyles = (inlineStyle = '') =>
+  inlineStyle.split(';').reduce((styleObject, stylePropertyValue) => {
+    // extract the style property name and value
+    let [property, value] = stylePropertyValue
+      .split(/^([^:]+):/)
+      .filter((val, i) => i > 0)
+      .map(item => item.trim().toLowerCase())
+
+    styleObject[property] = value
+    return styleObject
+  }, {})
