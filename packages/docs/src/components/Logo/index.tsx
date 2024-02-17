5a873ce41c58307cb61f38eb421088a9a2a96191
d.kambulov@napolke.ru <d.kambulov@napolke.ru>
2024-02-17 14:09:15 +0300

    test

--- packages/docs/src/components/Logo/index.tsx
+++ packages/docs/src/components/Logo/index.tsx
@@ -12,7 +12,7 @@ function Logo({ name }: Props) {
   return (
     <Link href="/" aria-label="napolke ui">
       <Text b>
-        <Image width="100px" height="50px" src={'../../images/svg/logo.svg'} />
+        <Image width="100px" height="50px" src={'../../images/svg/Logo.svg'} />
         {name && <div className="container">{name}</div>}
       </Text>
       <style jsx>{`
